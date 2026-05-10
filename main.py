d8e2860b13f1a05edb6353a9f1ccd45e63b4dbaf
HamedT-coder <shtahamip@gmail.com>
2026-05-10 10:42:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
-# Import libraries - به روش صحیح
 from rubka.asynco import Robot, filters
 from rubka.context import Message
 import asyncio
 from pytube import YouTube
 import os, re
-from dotenv import load_dotenv
-
-load_dotenv()
-token = os.getenv("BOT_TOKEN")
-bot = Robot(token)
+from downloader import download_video
 
 # Base variables
 DOWNLOAD_LOCATION = "./temp/"
 is_yt_sent = False
+token = os.getenv(BOT_TOKEN)
+bot = Robot(token)
 
 # Send welcome message to new users
 @bot.on_message(filters.is_command.start)
 async def start(bot:Robot, message:Message):
     await message.reply("به ربات آپارات دانلودر من خوش اومدی!")
-
-# Handle /yt command
-@bot.on_message()
-async def yt_command(bot:Robot, message:Message):
-    text = message.text
-    if text == "/yt":
-        global is_yt_sent
-        await message.reply("یه لینک برام بفرست تا دانلودش کنم!")
-        is_yt_sent = True
 
 # Download video from youtube and send to user
 @bot.on_message()
@@ -37,24 +25,23 @@ async def download(bot:Robot, message:Message):
     link = message.text
     pattern = r"http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌[\w\?‌=]*)?"
     result = re.match(pattern, link)
-    if result and message.is_user and is_yt_sent or result and message.is_group:
+    if result and message.is_user or result and message.is_group:
         try:
             # ارسال وضعیت در حال پردازش
             status_msg = await message.answer("در حال دانلود ویدیو... لطفاً صبر کنید ⏳")
         # Download video from youtube
-            youtube = YouTube(link)
-            youtube_stream = youtube.streams.get_highest_resolution()
-            file_path = youtube_stream.download(DOWNLOAD_LOCATION)
-        # Send video to user
-            file_name = youtube.streams.get_highest_resolution().default_filename
-            #file_dir = f"{DOWNLOAD_LOCATION}{file_name}"
+            video = download_video(
+                video_url=link.strip(),
+                output_type="video",
+                quality="720"
+            )
             with open(file_path, 'rb') as video_file:
                 await bot.send_file(
                 user_id,           
                 video=video_file,                                        caption="فایل آپلود شد!"
                 )
         # Delete video from disk after sending to user
-            os.remove(file_path)
+            os.remove(video)
             
             # حذف پیام وضعیت
             await status_msg.delete()
@@ -62,7 +49,7 @@ async def download(bot:Robot, message:Message):
         except Exception as e:
             await message.reply(f"❌ خطا در دانلود: {str(e)}")
             
-    elif result == False and is_yt_sent == True:
+    elif result == False:
         await message.reply("لینک نامعتبر!")
 
 bot.run()
